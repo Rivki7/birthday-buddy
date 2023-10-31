d49123302ee346641013b7a695d069cd0e1cb401
Rivki7 <rivkiii770@gmail.com>
2023-10-31 13:07:18 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+import data from './data';
+import People from './People';
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleClick = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <People people={people} />
+        <button type="button" className="btn btn-block" onClick={handleClick}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 };
 export default App;
